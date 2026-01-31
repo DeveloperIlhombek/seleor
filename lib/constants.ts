c6666fa31a8fed9e8d9760c28989e94e978c8307
DeveloperIlhombek <ilxomdeveloper@gmail.com>
2026-01-31 14:01:03 +0500

    sjbdj

---
+++ lib/constants.ts
@@ -0,0 +1,35 @@
+export const products = [
+	{
+		_id: '1',
+		title: 'product 1',
+		description: 'Description 1',
+		image: '/1.webp',
+		category: 'Category 1',
+		price: 100000,
+	},
+	{
+		_id: '2',
+		title: 'product 2',
+		description: 'Description 1',
+		image: '/2.webp',
+		category: 'Category 2',
+		price: 200000,
+	},
+	{
+		_id: '3',
+		title: 'product 3',
+		description: 'Description 3',
+		image: '/3.webp',
+		category: 'Category 2',
+		price: 300000,
+	},
+]
+export const categories = [
+	'All',
+	'Shoes',
+	'T-Shirts',
+	'Clothes',
+	'Books',
+	'Accessories',
+	'Universal',
+]
